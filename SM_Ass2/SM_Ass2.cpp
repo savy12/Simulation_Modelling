79c18968f1dc530da0a3eb033a37559926d4dac3
Sarvesh Arora <52189391+savy12@users.noreply.github.com>
2021-08-04 10:56:52 +0530

    Update SM_Ass2.cpp

--- SM_Ass2/SM_Ass2.cpp
+++ SM_Ass2/SM_Ass2.cpp
@@ -6,47 +6,37 @@ int main()
 {
     int P, Q, stock, today, dueDate, unitsDue, startDate, demand, reorderCost;
     float total_cost;
-
-    //For different values of P and Q
     for(int i=0;i<5;i++)
     {
-        cin>>P>>Q; //125 150
-        //initialize random seed for constant random values
+        cin>>P>>Q;
         srand(0);
 
-        //All the initializations
         startDate = 1;
         dueDate = 0;
         today = startDate;
         unitsDue = 0;
-        reorderCost = 2; //not given
+        reorderCost = 2; 
         stock = 115;
         total_cost=0;
 
         while(today<=180)
         {
-            //If due date, refill stock
             if(today==dueDate)
             {
                 stock+= Q;
                 unitsDue=0;
             }
-            //Today's demand of orders
             demand = rand() % 100;
-
-            //If demand less than stock, add carrying cost
             if(demand<=stock)
             {
                 stock-= demand;
                 total_cost+= stock*0.75;
             }
-            //If demand more than stock, add lost sale cost
             else
             {
                 total_cost+= (demand - stock)*18;
                 stock = 0;
             }
-            //If stock less than minimum level, restock order
             if(stock + unitsDue <= P)
             {
                 unitsDue = Q;
